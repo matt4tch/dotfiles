55e1ae1c3a26434cd3659a419b1444c42cdc6ada
Matthew Tchouikine <matthew4.tch@gmail.com>
2026-04-13 12:58:53 +0200

    feat: add typst preview support

---
+++ nvim/lua/plugins/typst-preview.lua
@@ -0,0 +1,22 @@
+return {
+  "chomosuke/typst-preview.nvim",
+  ft = "typst",
+  version = "1.*",
+  opts = {
+    open_cmd = "qutebrowser %s",
+    dependencies_bin = {
+      ["tinymist"] = "tinymist",
+    },
+  },
+  keys = {
+    { "<leader>tu", "<cmd>TypstPreviewUpdate<cr>", desc = "Typst: Update/refresh binaries" },
+    { "<leader>ts", "<cmd>TypstPreview document<cr>", desc = "Typst: Start preview (document)" },
+    { "<leader>tS", "<cmd>TypstPreview slide<cr>", desc = "Typst: Start preview (slide)" },
+    { "<leader>tq", "<cmd>TypstPreviewStop<cr>", desc = "Typst: Stop preview" },
+    { "<leader>tt", "<cmd>TypstPreviewToggle<cr>", desc = "Typst: Toggle preview" },
+    { "<leader>tfy", "<cmd>TypstPreviewFollowCursor<cr>", desc = "Typst: Follow cursor (enable)" },
+    { "<leader>tfn", "<cmd>TypstPreviewNoFollowCursor<cr>", desc = "Typst: Follow cursor (disable)" },
+    { "<leader>tft", "<cmd>TypstPreviewFollowCursorToggle<cr>", desc = "Typst: Follow cursor (toggle)" },
+    { "<leader>tc", "<cmd>TypstPreviewSyncCursor<cr>", desc = "Typst: Sync preview to cursor" },
+  },
+}
