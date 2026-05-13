afce0c2a4d0de77ebc170db0c9a7f59915231598
Matthew Tchouikine <matthew4.tch@gmail.com>
2026-05-13 08:51:41 -0400

    chore: update local dotfiles config

--- nvim/lua/plugins/typst-preview.lua
+++ nvim/lua/plugins/typst-preview.lua
@@ -7,6 +7,20 @@ return {
     dependencies_bin = {
       ["tinymist"] = "tinymist",
     },
+    get_root = function(path_of_main_file)
+      local root = os.getenv("TYPST_ROOT")
+      if root then
+        return root
+      end
+
+      local main_dir = vim.fs.dirname(vim.fn.fnamemodify(path_of_main_file, ":p"))
+      local found = vim.fs.find({ ".typst-root", "typst.toml", ".git" }, { path = main_dir, upward = true })
+      if #found > 0 then
+        return vim.fs.dirname(found[1])
+      end
+
+      return main_dir
+    end,
   },
   keys = {
     { "<leader>tu", "<cmd>TypstPreviewUpdate<cr>", desc = "Typst: Update/refresh binaries" },
